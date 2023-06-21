f995e8e80337f351398212d53c708dcc29306dd4
johnnguyenexp39 <127367857+johnnguyenexp39@users.noreply.github.com>
2023-06-21 14:05:34 +0700

    updated

--- lib/data/projects.dart
+++ lib/data/projects.dart
@@ -9,12 +9,27 @@ class Project {
 }
 
 // ignore: non_constant_identifier_names
+
 List<Project> PROJECTS = [
+  Project(
+    name: 'Freya',
+    description:
+        "A clinic booking app allows users to conveniently schedule appointments with healthcare providers. It provides a user-friendly interface where patients can search for available doctors, select a preferred date and time, and book appointments. The app may also include features such as doctor profiles, patient reminders, online payment options, and the ability to manage and reschedule appointments. Overall, the app streamlines the process of booking and managing clinic appointments, enhancing accessibility and convenience for patients.",
+    image: 'images/freya.png',
+    url: 'https://freya-c23c4.web.app/',
+    skills: [
+      'Js',
+      'React Navite',
+      'Golang',
+      'Micro Service',
+      'Redis Caching',
+    ],
+  ),
   Project(
     name: 'PimsPoints',
     description:
         "PimsPoints was created by an educator who understands that COMMUNICATION, OPPORTUNITY, and ACCOUNTABILITY are key factors that contribute to effective parent engagement. These three factors became the framework on which PimsPoints was designed. It is because of the conceptual framework of the technology that schools, non-profits, and parents are able to determine parent engagement activity in a meaningful and REWARDING way.",
-    image: 'images/projects/pimspoints.png',
+    image: 'images/pimspoints.png',
     url: 'https://www.pimspoints.com/',
     skills: [
       'Dart',
@@ -28,7 +43,7 @@ List<Project> PROJECTS = [
     name: 'Chocolate',
     description:
         "The Chocolate application is a financial application that enables its users to make deposits into the system and receive monthly interest rates that surpass those offered by central or commercial banks.",
-    image: 'images/projects/chocofin.png',
+    image: 'images/chocofin.png',
     url: 'https://www.pimspoints.com/',
     skills: [
       'Flutter',
@@ -42,7 +57,7 @@ List<Project> PROJECTS = [
     name: 'TMail',
     description:
         "An Opensource Project distributed by Linagora, the application has similar feature as Gmail.",
-    image: 'images/projects/tmail.png',
+    image: 'images/tmail.png',
     url: 'https://github.com/linagora/tmail-flutter',
     skills: [
       'Dart',
@@ -57,7 +72,7 @@ List<Project> PROJECTS = [
     name: 'NameBook',
     description:
         "A FLutter application like facebook messenger which has audio / video calling, chat messenger and other features,  this app used for japanese company that need an app to communicate between employees",
-    image: 'images/projects/namebook.jpeg',
+    image: 'images/namebook.jpeg',
     url: '',
     skills: [
       'Dart',
@@ -74,7 +89,7 @@ List<Project> PROJECTS = [
     name: 'Sellove',
     description:
         "Sellove is the free broadcasting app and streaming tool for creators for you to sell stuffs by using your camera, go live with just a few taps to the largest social live video streaming platforms! Chat with people all over the world as they watch you experience wonderful products and broadcast everyday adventures in real-time",
-    image: 'images/projects/sellove.jpeg',
+    image: 'images/sellove.jpeg',
     url: 'https://play.google.com/store/apps/details?id=com.sellove.service',
     skills: [
       'Dart',
@@ -89,7 +104,7 @@ List<Project> PROJECTS = [
     name: 'CustomDat',
     description:
         'CustomDat is a application allows people to customize T-shirt, Mugs, Cases, ... then those designs will be printed and delivery to users',
-    image: 'images/projects/customdat.png',
+    image: 'images/customdat.png',
     url: 'https://apps.apple.com/es/app/customdat/id1286539618',
     skills: [
       'Swift',
@@ -102,7 +117,7 @@ List<Project> PROJECTS = [
     name: 'Memvisit',
     description:
         'Memvisit mobile app is part of healthcare communication platform. It allows you to connect with your beloved ones in a secure, simple manner, plus smooth user experience.With the mission to connect people, encourage everyone to spend more time for family members especially the child-parent relationship. This app is a communication tool in healthcare, where anyone can keep contactable, updated more regularly between doctors, patients and their relatives.',
-    image: 'images/projects/memvisit.jpeg',
+    image: 'images/memvisit.jpeg',
     url: 'https://apps.apple.com/app/memvisit/id1458578295',
     skills: [
       'Swift',
